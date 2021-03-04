c715b3347130bf0cee22b7322c6f89eeabb41ee2
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2021-03-04 14:41:09 +0100

    Add project files.

---
+++ TakeNote.Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeNote.Models
+{
+    class Card
+    {
+        public string Id { get; set; }
+        public Item FrontFace { get; set; }
+        public Item BackFace { get; set; }
+    }
+}
---
+++ TakeNote.Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeNote.Models
+{
+    class Question
+    {
+        public string Id { get; set; }
+        public Item Inquery { get; set; }
+        public List<string> Answers { get; set; }
+        public int AnswerIndex { get; set; }
+    }
+}
